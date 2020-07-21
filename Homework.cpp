2a470b14b22a78a56826fd46e5533a11a4af7ff1
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-21 13:58:45 +0700

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -5,11 +5,11 @@ void main()
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl ;
 	cout << "1st Term of 2020         \\45132114 Wanthanee Prachuabsupakij" << endl ;
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl ;
-	cout << "| Subject ID | Subject name             | Score (100)| Grade |" << endl ;
+	cout << "| Subject ID | Subject name\t\t| Score (100)\t| \tGrade\t|" << endl ;
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl ;
-	cout << "|  4570034   | Introduction to computer |     85     |  \"A\"  |" << endl ;
-	cout << "|  4580247   | English                  |     74     |  \"B\"  |" << endl ;
-	cout << "|  4530001   | Design Thinking          |     92     |  \"A\"  |" << endl ;
+	cout << "|  4570034   | Introduction to computer |\t85\t|\t\"A\"\t|" << endl ;
+	cout << "|  4580247   | English\t\t\t|\t74\t|\t\"B\"\t|" << endl ;
+	cout << "|  4530001   | Design Thinking\t\t|\t92\t|\t\"A\"\t|" << endl ;
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl ;
 	cout << "|TOTAL| Subject = 3                     | Average = 3.87|" << endl ;
 	cout << "==================================*===" << endl << endl << endl ;
