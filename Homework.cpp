c53472f0b2f4b1d816ed4488536cca73975a285b
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-21 13:59:13 +0700

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -11,7 +11,7 @@ void main()
 	cout << "|  4580247   | English\t\t\t|\t74\t|\t\"B\"\t|" << endl ;
 	cout << "|  4530001   | Design Thinking\t\t|\t92\t|\t\"A\"\t|" << endl ;
 	cout << "-----------------------------------------------------------------------------------------------------------------------" << endl ;
-	cout << "|TOTAL| Subject = 3                     | Average = 3.87|" << endl ;
+	cout << "|TOTAL| Subject = 3\t\t\t| Average = 3.87|" << endl ;
 	cout << "==================================*===" << endl << endl << endl ;
 	cout << "KMUTNB" << endl ;
 	system ("pause");
