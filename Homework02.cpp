095d836e7385cc49a3cc2abe70c28f11c3f3cf82
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-21 13:56:10 +0700

    Add files via upload

---
+++ Homework02.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void main()
+{
+	cout<<"********************Homework********************\n";
+	string name , surname;
+	int salary , sale , com_percent , total ;
+
+	cout<<"Enter Name \t\t: ";
+	cin>>name>>surname;
+	cout<<"Enter Salary \t\t: ";
+	cin>>salary;
+	cout<<"Enter Sale \t\t: ";
+	cin>>sale;
+	cout<<"Commission Percent \t: ";
+	cin>>com_percent;
+
+	cout<<"------------------------------------------------\n";
+	cout<<"Your name = "<<name<<" "<<surname<<endl;
+
+	total = salary + (sale*com_percent/100);
+	cout<<"Total Revenue "<<total<<" Bath\n";
+	cout<<"------------------------------------------------\n";
+
+}
